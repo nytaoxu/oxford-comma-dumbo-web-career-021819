6ac99a1ea661015f4530ac3c2408f5603ff75eb6
Tao Xu <nytaoxu@gmail.com>
2019-02-10 20:43:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@ def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
   end
-  array.last = "and " + array.last
+  # array.last = "and " + array.last
+  array[-1] = "and " + array[-1]
   array.join(", ")
 end
